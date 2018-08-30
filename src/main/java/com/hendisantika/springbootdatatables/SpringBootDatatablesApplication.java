79a2d817140a4ab8376ff2fc5dc1dd8d56f7308c
Hendi Santika <hendisantika@gmail.com>
2018-08-31 06:55:09 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootdatatables/SpringBootDatatablesApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootdatatables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatatablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatatablesApplication.class, args);
+    }
+}
