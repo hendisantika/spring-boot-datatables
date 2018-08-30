4e53e86bb7076acb39ab145257cd0d6f1267e84d
Hendi Santika <hendisantika@gmail.com>
2018-08-31 06:58:51 +0700

    #4 - Add Employee Entity

---
+++ src/main/java/com/hendisantika/springbootdatatables/Employee.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootdatatables;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-datatables
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/08/18
+ * Time: 06.57
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int age;
+    private int salary;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_office")
+    private Office office;
+
+}
