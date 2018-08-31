4dbed6007ff2f2e7e87341d023da7d80d17d96b0
Hendi Santika <hendisantika@gmail.com>
2018-08-31 07:02:19 +0700

    #8 - Add jquery.spring-friendly.js file

--- src/main/java/com/hendisantika/springbootdatatables/SpringBootDatatablesApplication.java
+++ src/main/java/com/hendisantika/springbootdatatables/SpringBootDatatablesApplication.java
@@ -2,8 +2,11 @@ package com.hendisantika.springbootdatatables;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 public class SpringBootDatatablesApplication {
 
     public static void main(String[] args) {
